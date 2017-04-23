30acdb23620c4dcce38f3eb32803f92592964d20
JulianMar11 <julian.marstaller@online.de>
2017-04-23 18:27:54 +0200

    Resultsadded

--- survey/views.py
+++ survey/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
   context = {}
@@ -11,3 +12,8 @@ def interests(request):
 def skills(request):
   context = {}
   return render(request, 'survey/skills.html', context)
+
+def results(request):
+  matched_services_list = Service.objects.all()
+  context = {'matched_services_list':  matched_services_list}
+  return render(request, 'survey/results.html', context)
