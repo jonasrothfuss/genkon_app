b37e09efbf41544dbbef33f16750d9fbec70296b
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-04-23 17:17:24 +0200

    Admin Views

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+  fields = ['question_text', 'question_type']
+  inlines = [ChoiceInline]
+
+class ProfileAdmin(admin.ModelAdmin):
+  pass
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Profile)
+admin.site.register(Service)
