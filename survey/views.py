c337a41dcbb25f4426288dca3b8ec7dc7ec15b5f
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-04-23 20:23:23 +0200

    render skills forms dynamically

--- survey/views.py
+++ survey/views.py
@@ -10,7 +10,15 @@ def interests(request):
   return render(request, 'survey/interests.html', context)
 
 def skills(request):
-  context = {}
+  question1 = Question.objects.get(question_identifier="skills1")
+  question2 = Question.objects.get(question_identifier="skills2")
+  choices1 = question1.get_choices()
+  choices2 = question2.get_choices()
+  number_of_dropdown_skills1 = 3
+  assert len(choices1) > 0 and len(choices2) > 0
+
+  context = {'question1': question1, 'choices1': choices1, 'question2': question2,
+             'choices2': choices2, 'dropdown_iterator_skills1': range(number_of_dropdown_skills1)}
   return render(request, 'survey/skills.html', context)
 
 def results(request):
