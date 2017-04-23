d408c694e4321ab06203d87dc06156f6a38e4578
JulianMar11 <julian.marstaller@online.de>
2017-04-23 14:50:56 +0200

    newHtmlLink

--- survey/urls.py
+++ survey/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^interests$', views.interests, name='interests'),
 ]
--- survey/views.py
+++ survey/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
   context = {}
   return render(request, 'survey/index.html', context)
+
+def interests(request):
+  context = {}
+  return render(request, 'survey/interests.html', context)
