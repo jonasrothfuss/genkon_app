d317343c721e6131caa334b6aeb7d3ce6fb77640
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-05-01 17:56:11 +0200

    Session flow control

--- survey/views.py
+++ survey/views.py
@@ -29,42 +29,51 @@ def skills(request, num_selectors_skills1=3):
   question1 = Question.objects.get(question_identifier="skills1")
   question2 = Question.objects.get(question_identifier="skills2")
 
-  if request.method == 'POST':
-    form1 = SkillsForm1(request.POST, num_selectors=num_selectors_skills1)
-    form2 = SkillsForm2(request.POST)
-    if form1.is_valid() and form2.is_valid():
-      request.session['skills_post'] = request.POST
-      return HttpResponseRedirect(reverse('results'))
+  if 'interests_post' in request.session: #assure that interests have been provided
+    if request.method == 'POST':
+      form1 = SkillsForm1(request.POST, num_selectors=num_selectors_skills1)
+      form2 = SkillsForm2(request.POST)
+      if form1.is_valid() and form2.is_valid():
+        request.session['skills_post'] = request.POST
+        return HttpResponseRedirect(reverse('results'))
+      else:
+        form1 = SkillsForm1(num_selectors=num_selectors_skills1)
+        form2 = SkillsForm2()
+
     else:
       form1 = SkillsForm1(num_selectors=num_selectors_skills1)
       form2 = SkillsForm2()
 
-  else:
-    form1 = SkillsForm1(num_selectors=num_selectors_skills1)
-    form2 = SkillsForm2()
+    context = {'form1': form1, 'form2': form2, 'question1': question1, 'question2': question2}
+    return render(request, 'survey/skills.html', context)
 
-  context = {'form1': form1, 'form2': form2, 'question1': question1, 'question2': question2}
-  return render(request, 'survey/skills.html', context)
+  else: #interests were not provided yet, redirect to interests view
+    return HttpResponseRedirect(reverse('interests'))
 
 def results(request):
-  print(request.session['interests_post'])
-  print(request.session['skills_post'])
-
   matched_services_list = Service.objects.all()[:4]
   context = {'matched_services_list':  matched_services_list}
   return render(request, 'survey/results.html', context)
 
 def profile_data(request):
-  if request.method == 'POST':
-    form = ProfileDataForm(request.POST)
-    if form.is_valid():
-      form.save()
-      return HttpResponseRedirect(reverse('results'))
+  """ Check if skill and interest form data is stored in session """
+  if 'interests_post' not in request.session:  #interests were not provided yet, redirect to interests view
+    return HttpResponseRedirect(reverse('interests'))
+  elif 'skills_post' not in request.session: #skills were not provided yet, redirect to skills view
+    return HttpResponseRedirect(reverse('skills'))
+
+  else: #interests and skills are provided --> proceed with profile data
+    print(request.session['interests_post'], request.session['skills_post'])
+    if request.method == 'POST':
+      form = ProfileDataForm(request.POST)
+      if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('results'))
+      else:
+        form = ProfileDataForm()
+
     else:
       form = ProfileDataForm()
 
-  else:
-    form = ProfileDataForm()
-
-  context = {'form': form}
-  return render(request, 'survey/profile_data.html', context)
+    context = {'form': form}
+    return render(request, 'survey/profile_data.html', context)
