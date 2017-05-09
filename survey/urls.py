4593821e3cc2e1d171d8fbb0b3c0ca89f8bd5670
Dominik Schulz <d.dominik.schulz@gmail.com>
2017-05-10 00:50:54 +0200

    Add url routing for profile list and account pages

--- survey/urls.py
+++ survey/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
 
 from . import views
 
@@ -11,4 +12,6 @@ urlpatterns = [
     url(r'^results$', views.results, name='results'),
     url(r'^profile_data$', views.profile_data, name='profile_data'),
     url(r'^thank_you', views.thank_you_note, name='thank_you_note'),
+    url(r'^list_profiles', views.ListProfilesView.as_view(), name='list_profiles'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
