97c0cb9db7c38085852a6d5cb9ace5bf70ea276e
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-06-06 20:41:17 +0200

    Settings management for development and production

---
+++ genkon_app/settings_conf/development.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '05)y=!cu*o(8b)y+#lri+(rydh4j%cesnhh%#@wj^xoz=pu_4='
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
