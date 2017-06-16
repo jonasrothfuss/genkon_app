3d2eebfa94da59317ea96bfb503daa344ff28154
JulianMar11 <julian.marstaller@online.de>
2017-06-16 18:45:35 +0200

    List_profiles in profile_list umbenannt

--- survey/urls.py
+++ survey/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^results$', views.results, name='results'),
     url(r'^profile_data$', views.profile_data, name='profile_data'),
     url(r'^thank_you', views.thank_you_note, name='thank_you_note'),
-    url(r'^list_profiles', views.ListProfilesView.as_view(), name='list_profiles'),
+    url(r'^profile_list', views.ListProfilesView, name='profile_list'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^profile_csv', views.profile_csv, name='profile_csv'),
     url(r'^profile_detail', views.profile_detail, name='profile_detail'),
